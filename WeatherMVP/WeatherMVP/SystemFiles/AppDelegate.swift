d075875843b10c9f5f71c6e6638997084362de18
hugo <hugostar@zoho.com>
2018-11-22 11:41:52 +0300

    create project

---
+++ WeatherMVP/WeatherMVP/SystemFiles/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WeatherMVP
+//
+//  Created by Мишустин Сергеевич on 22/11/2018.
+//  Copyright © 2018 LTD Zebka. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
